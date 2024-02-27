96a35ff84672d8f626d58f8ca280b118a00bb785
ameel <ameelaziz@gmail.com>
2024-02-26 19:27:14 -0600

    add everything

---
+++ backend/backend_app/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class MyModel(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    email = models.EmailField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
