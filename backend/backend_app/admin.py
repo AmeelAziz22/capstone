4b686ba8e337bc1852afcd1d2783da7c1ae2942d
ameelaziz <ameelaziz@tamu.edu>
2024-03-25 15:21:37 -0500

    created new model

--- backend/backend_app/admin.py
+++ backend/backend_app/admin.py
@@ -3,6 +3,7 @@ from .models import MyModel
 from .models import User
 from .models import Account_Stock
 from .models import PortfolioHistory
+from .models import StockPrediction
 
 admin.site.register(MyModel)
 
@@ -11,4 +12,6 @@ admin.site.register(User)
 admin.site.register(Account_Stock)
 
 admin.site.register(PortfolioHistory)
+
+admin.site.register(StockPrediction)
 
