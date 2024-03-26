6f2d605f8632b1f06228cfa9d76cf056ea074851
GrickRimes <elierdman@tamu.edu>
2024-03-26 15:39:28 -0500

    minor fix

--- backend/backend/stock_ml/daily.py
+++ backend/backend/stock_ml/daily.py
@@ -9,7 +9,7 @@ def job():
 
 def repeater():
     x=datetime.today()
-    y = x.replace(day=x.day, hour=x.hour, minute=x.minute, second=x.second+5, microsecond=0) #+ timedelta(days=1)
+    y = x.replace(day=x.day, hour=3, minute=0, second=0, microsecond=0) #+ timedelta(days=1)
     delta_t=y-x
 
     secs=delta_t.seconds+1
