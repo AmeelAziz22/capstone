83dc51c72245274c5c0bb0dfbdfaa4a6f84dcff4
GrickRimes <elierdman@tamu.edu>
2024-04-23 15:56:56 -0500

    Changed manage.py to run daily jobs. daily.py now runs LSTM_train.py.

--- backend/backend/daily.py
+++ backend/backend/daily.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta
 from threading import Timer
 import time
-# from backend.stock_ml import preprocess_data as ml
+from backend import LSTM_train as ml
 
 def job():
     ml.main()
@@ -11,7 +11,6 @@ def repeater():
     x=datetime.today()
     y = x.replace(day=x.day, hour=3, minute=0, second=0, microsecond=0) #+ timedelta(days=1)
     delta_t=y-x
-
     secs=delta_t.seconds+1
     print(secs)
     time.sleep(secs)
--- backend/manage.py
+++ backend/manage.py
@@ -3,25 +3,29 @@
 import os
 import sys
 import multiprocessing
-from backend.stock_ml import daily as d
-def main():
-    """Run administrative tasks."""
+from backend import daily as d
+
+def start_server():
+    """Start Django server."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
     try:
         from django.core.management import execute_from_command_line
+        execute_from_command_line(['manage.py', 'runserver'])
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    
-    # dailyProcess = multiprocessing.Process(target=d.main)
-    # dailyProcess.start()
 
-    execute_from_command_line(sys.argv)
+def main():
+    """Run administrative tasks."""
+    # Create a multiprocessing Process for running the server
+    server_process = multiprocessing.Process(target=start_server)
+    server_process.start()
 
-    # dailyProcess.join()
+    # Call daily's main function
+    d.main()
 
 if __name__ == '__main__':
     main()
